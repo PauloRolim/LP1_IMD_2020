d884db0a32f6e6936aaba062cfb169400a07780c
Paulo Rolim <41303759+PauloRolim@users.noreply.github.com>
2020-03-16 21:45:40 -0300

    Configurando dos atributos

---
+++ sapos/Sapo.cpp
@@ -0,0 +1,33 @@
+#include "Sapo.h"
+#include <iostream>
+#include <cstdlib>
+
+using namespace std;
+
+int Sapo::getIdendificador(){
+  return identificador;
+}
+
+int Sapo::getDistancia_percorrida(){
+  return distancia_percorrida;
+}
+
+int Sapo::getQuantidade_pulos_dados(){
+  return quantidade_pulos_dados;
+}
+
+void Sapo::setIdentificador(int var_ident){
+  identificador = var_ident;
+}
+
+void Sapo::setDistancia_percorrida(int var_distancia){
+  distancia_percorrida = var_distancia;
+}
+
+void Sapo::setQuantidade_pulos_dados(int var_pulos){
+  quantidade_pulos_dados = var_pulos;
+}
+
+int Sapo::pular(){
+  distancia_percorrida = rand() % 10;
+}
