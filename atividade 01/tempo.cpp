fa4a8fed515628738ad99fd9feca2640fde79418
PauloRolim <vandelldimitri@hotmail.com>
2020-06-19 11:44:21 -0300

    codigo de apoio

---
+++ atividade 01/tempo.cpp
@@ -0,0 +1,26 @@
+#include <ctime>
+#include <fstream>
+#include <iostream>
+#include <string>
+
+std::string format_current_date(const std::string &format)
+{
+    std::time_t time = std::time(nullptr);
+    char result[1024];
+
+    std::strftime(result, sizeof(result), format.c_str(), std::localtime(&time));
+
+    return std::string(result);
+}
+
+std::string get_current_date() { return format_current_date("%d/%m/%Y"); }
+
+std::string get_current_time() { return format_current_date("%H:%M:%S"); }
+
+int main(int argc, char *argv[])
+{
+    std::cout << get_current_date() << std::endl;
+    std::cout << get_current_time() << std::endl;
+
+    return 0;
+}
