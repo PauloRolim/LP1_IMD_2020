2f6f4ef2b55656e1da384328c9ada81eff8942c3
PauloRolim <vandelldimitri@hotmail.com>
2020-06-24 10:01:49 -0300

    Correções

--- aula06/src/Diary.cpp
+++ aula06/src/Diary.cpp
@@ -3,19 +3,27 @@
 
 Diary::Diary(const std::string& nome_arquivo){
 
-    this -> mensagem_capacidade = 50;
+    this -> mensagem_capacidade = 10;
     this -> mensagem_tamanho = 0;
     this -> nome_arquivo = nome_arquivo;
+    this -> mensagens = nullptr;
 
+    mensagens = new Mensagem[mensagem_capacidade]; //alocação dinâmica de mensagens
 }
 
 void Diary::add(const std::string& mensagem){
 
-    this -> mensagens[this->mensagem_tamanho] = diary.mensagem_tamanho;
+    Mensagem m1; //variavel de inicialização
+
+    m1.conteudo = mensagem;
+
+    this -> mensagens[this->mensagem_tamanho] = m1;
     this -> mensagem_tamanho +=1;
 
 }
 
 void Diary::write(){
+
+
 
 }
